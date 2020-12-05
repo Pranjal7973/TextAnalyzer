3e66bdcb5716be2d24697bd46160f814b42c58b7
Pranjal Shrivastav <pranjalsrivastav07@gmail.com>
2020-12-05 18:18:48 +0530

    responsive

--- index.js
+++ index.js
@@ -1,90 +1,105 @@
 
+var submit = document.getElementById('submit');
+submit.addEventListener('click', clicked)
 
 
 function clicked() {
 
+
     var inputText = document.getElementById('inputText').value;
+    if (inputText !== '') {
 
-    var newVal = inputText.split(' ')
-    var count = 0;
-    var comma = 0;
-    var space = 0;
-    var period = 0;
-    var colon = 0;
-    var exclamation = 0;
-    var questionMark = 0;
 
-    var str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var newVal = inputText.split(' ')
+        var count = 0;
+        var comma = 0;
+        var space = 0;
+        var period = 0;
+        var colon = 0;
+        var exclamation = 0;
+        var questionMark = 0;
 
-    // debugger
+        var str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    for (let index = 0; index < inputText.length; index++) {
-        if (inputText[index] === ' ') {
-            space += 1
+        // debugger
+
+        for (let index = 0; index < inputText.length; index++) {
+            if (inputText[index] === ' ') {
+                space += 1
+            }
+
         }
 
+        newVal.forEach(element => {
+
+            for (var i = 0; i < str.length; i++) {
+                if (element.startsWith(str[i])) {
+                    count += 1;
+                }
+            }
+
+
+            for (var i = 0; i < element.length; i++) {
+                if (element[i] === ',') {
+                    // debugger
+                    comma += 1;
+                }
+                else if (element[i] === '.') {
+                    period += 1;
+                }
+                else if (element[i] === ':') {
+                    colon += 1;
+                }
+                else if (element[i] === '!') {
+                    exclamation += 1;
+                }
+                else if (element[i] === '?') {
+                    questionMark += 1;
+                }
+            }
+        });
+
+
+
+        var resultComma = document.getElementById('comma')
+        var resultPeriod = document.getElementById('period')
+        var resultQuestion = document.getElementById('question')
+        var resultExclamation = document.getElementById('exclamation')
+        var resultColon = document.getElementById('colon')
+        var resultSpace = document.getElementById('space')
+        var words = document.getElementById('words')
+
+        words.innerHTML = ` Total Words : ${count}`;
+        resultColon.innerHTML = `Colons : ${colon}`;
+        resultComma.innerHTML = `Commas : ${comma}`;
+        resultPeriod.innerHTML = `Dots : ${period}`;
+        resultQuestion.innerHTML = `Question Marks : ${questionMark}`;
+        resultExclamation.innerHTML = `Exclamation Marks : ${exclamation}`;
+        resultSpace.innerHTML = `Spaces : ${space}`
+
+
+        var results = document.getElementById('results')
+        results.style.display = 'block'
+        document.getElementById('body').style.height = '140vh'
+        window.location.replace('index.html#results')
+
+    } else {
+        Swal.fire({
+            icon: 'warning',
+            title: 'WAIT!',
+            text: 'Input Field Cannot be Empty',
+            confirmButtonColor: 'rgb(11, 235, 235)',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
     }
-
-    newVal.forEach(element => {
-
-        for (var i = 0; i < str.length; i++) {
-            if (element.startsWith(str[i])) {
-                count += 1;
-            }
-        }
-
-
-        for (var i = 0; i < element.length; i++) {
-            if (element[i] === ',') {
-                // debugger
-                comma += 1;
-            }
-            else if (element[i] === '.') {
-                period += 1;
-            }
-            else if (element[i] === ':') {
-                colon += 1;
-            }
-            else if (element[i] === '!') {
-                exclamation += 1;
-            }
-            else if (element[i] === '?') {
-                questionMark += 1;
-            }
-        }
-    });
-
-
-
-    var resultComma = document.getElementById('comma')
-    var resultPeriod = document.getElementById('period')
-    var resultQuestion = document.getElementById('question')
-    var resultExclamation = document.getElementById('exclamation')
-    var resultColon = document.getElementById('colon')
-    var resultSpace = document.getElementById('space')
-    var words = document.getElementById('words')
-
-    words.innerHTML = ` Total Words : ${count}`;
-    resultColon.innerHTML = `Colons : ${colon}`;
-    resultComma.innerHTML = `Commas : ${comma}`;
-    resultPeriod.innerHTML = `Dots : ${period}`;
-    resultQuestion.innerHTML = `Question Marks : ${questionMark}`;
-    resultExclamation.innerHTML = `Exclamation : ${exclamation}`;
-    resultSpace.innerHTML = `Spaces : ${space}`
-
-
-    var results = document.getElementById('results')
-    results.style.display = 'block'
-    document.getElementById('body').style.height = '140vh'
-    window.location.replace('index.html#results')
-
 }
 
 
-
-var submit = document.getElementById('submit');
-
-submit.addEventListener('click', clicked)
 submit.addEventListener('mouseout', () => {
     submit.style.transition = 'all 0.4s ease-out'
 })
@@ -98,6 +113,6 @@ reset.addEventListener('click', () => {
 
 })
 
-reset.addEventListener('mouseout', ()=>{
+reset.addEventListener('mouseout', () => {
     reset.style.transition = 'all 0.4s ease-out'
 })
